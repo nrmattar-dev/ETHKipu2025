f70a9f9b4df0c02ff899f7b7b599290408b858ff
nrmattar-dev <nrmattar.dev@gmail.com>
2025-06-08 12:28:19 -0300

    ETHKipu - TP 2 - Subasta

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -64,53 +64,53 @@ contract Auction{
     }
 
     //---------------------------------------------------------//
-    function MakeABid () payable isActive() isHigherThanGap(msg.value)  external {
+    function MakeABid () payable isActive() isNotEnded() external {
         
-        //Si aún el tiempo está vigente, no puede terminar la subasta
-        require(block.timestamp<finishDate,"The Auction has ended");
+        //Valido que la oferta sea mayor a 0.
+        require (msg.value > 0, "The amount must be higher than zero");
 
-        if (msg.value > winnerBid.amount){
-            //Asigno que el valor corresponde a la address del sender
-            bidAddressLastAmount[msg.sender] = msg.value;
-            bidAddressTotalAmount[msg.sender] += msg.value;
-            //adiciono su apuesta a la colección
-            bids.push(Bid(msg.sender,msg.value));
-
-            //Actualizo las variables de la oferta ganadora al momento
-            winnerBid.amount = msg.value;
-            winnerBid.bidder = msg.sender;
-            //Emito el evento de una nueva oferta
-            emit NewOffer(msg.sender, msg.value);
-            //Chequeo si está cerca del final de la subasta y corro la fecha de fin
-            if (block.timestamp >= (finishDate-bidExtensionZone))
-            {
-                finishDate = block.timestamp + bidExtensionTime;
-            }
-        }
-    }  
-
-    modifier isHigherThanGap(uint256 _amount){
         /*
         Como Solidity no maneja decimales, en lugar de dividir para validar, debo multiplicar.
         Ejemplo: Si _amount es 200, maxBidAmount es 150 y bidGap es 5, entonces:
         200*100 >= 150 * (100 + 5)
         20000 >= 15750 --> OK
         */
-        require (_amount*100 >= winnerBid.amount*(100 + bidGap), "The amount is not higher than the gap");
-        _;
-    }
 
-    //Valido que la subasta esté vigente
-    modifier isEnded() {
-        //Si aún el tiempo está vigente, no puede terminar la subasta
-        require(block.timestamp>finishDate,"The Auction has not ended");
-        _;
-    } 
+	    //Valido que el valor sea superior al menos un bidGap% que la oferta ganadora para tomarlo.
+        require (msg.value*100 >= winnerBid.amount*(100 + bidGap), "The amount is not higher than the gap");
+    
+        //Guardo la información de este nuevo postor con su oferta superadora
+        bidAddressLastAmount[msg.sender] = msg.value;
+        bidAddressTotalAmount[msg.sender] += msg.value;
+        bids.push(Bid(msg.sender,msg.value));
+
+        //Actualizo la variable estructurada winnerBid con la info de la oferta superadora.
+        winnerBid.amount = msg.value;
+        winnerBid.bidder = msg.sender;
+
+        //Emito el evento de que hay una oferta superadora
+        emit NewOffer(msg.sender, msg.value);
+
+        //Chequeo si está cerca del final de la subasta y, de ser así, corro la fecha de fin
+        if (block.timestamp >= (finishDate-bidExtensionZone))
+        {
+            finishDate = block.timestamp + bidExtensionTime;
+        }
+
+    }  
 
     event NewOffer(address indexed sender, uint256 value);
 
+    //Valido que la subasta esté vigente
+    modifier isNotEnded() {
+        //Si aún el tiempo está vigente, no puede terminar la subasta
+        require(block.timestamp<finishDate,"The Auction has ended");
+        _;
+    }
+
     //---------------------------------------------------------//
 
+        
     function EndAuction () isActive() isEnded() onlyOwner() external {
 
         active = false;
@@ -118,29 +118,39 @@ contract Auction{
         uint256 refundAmount = 0;
         address bidder;
 
+        //Recorro la colección de ofertantes
         for (uint i=0; i < bids.length; i++)
         {
-            bidder = bids[i].bidder;
-            totalAmount = bidAddressTotalAmount[bidder];
-            bidAddressTotalAmount[bidder] = 0; //Una vez que transfiero, vacío su totalamount.
+        
+            bidder = bids[i].bidder; //Cargo el address
+            totalAmount = bidAddressTotalAmount[bidder]; //Cargo el monto total acumulado de todas sus ofertas
+            
+        //Si tiene dinero para transferir, procedo.
+        if (totalAmount > 0) 
+        {
 
-            if (bidder != winnerBid.bidder) //si no es el ganador, le devuelvo
+            bidAddressTotalAmount[bidder] = 0; //Como le transfiero el total, vacío la variable para que no siga entrando (si tuvo diversas pujas, seguiría entrando).
+        
+            //Verifico si se trata o no del ganador.
+            if (bidder != winnerBid.bidder) 
             {
+                //si no es el ganador, le devuelvo el dinero descontándole la comisión del gas.
                 refundAmount = totalAmount - ((totalAmount*gasCommissionPercentage)/100);
                 (bool sent,) = payable(bidder).call{value: refundAmount}(""); //Transfiero
                 require(sent, "Failed to refund"); //Si hubo error, revierto.                
             }
-            else //si es el ganador, le transfiero el monto al vendedor
+            else 
             {
+                //si es el ganador, transfiero la totalidad al vendedor.
                 (bool sent,) = payable(seller).call{value: totalAmount}(""); //Transfiero
                 require(sent, "Failed to tranfer prize"); //Si hubo error, revierto.                
+            }
             }
 
         }
 
         emit AuctionEnded(winnerBid);
     }
-
 
     event AuctionEnded(Bid winnerBid);
 
@@ -151,33 +161,41 @@ contract Auction{
     }
 
     //Valido que la subasta esté vigente
+    modifier isEnded() {
+        //Si aún el tiempo está vigente, no puede terminar la subasta
+        require(block.timestamp>finishDate,"The Auction has not ended");
+        _;
+    }     
+
+    //Valido que la subasta esté vigente
     modifier isActive() {
         require(active==true,"The Auction is not longer active");
         _;
     }  
  
     //---------------------------------------------------------//
-    function PartialRefund() external isActive() {
+    function PartialRefund() external isActive() isNotEnded()  {
         uint256 lastAmount = bidAddressLastAmount[msg.sender];
         uint256 totalAmount = bidAddressTotalAmount[msg.sender];
-
+		
+		//Si el monto que tiene acumulado es 0, no hago nada porque significa que no se recibieron ofertas de él/ella
         if (totalAmount == 0)
         {
             revert("No bids were received from you"); 
         }
 
+		//Si la última oferta es igual al acumulado entonces tampoco le devuelvo nada porque significa o que sólo hizo una oferta o que ya se le devolvió de forma parcial anteriormente.
         if (lastAmount == totalAmount)
         {
             revert("There is no partial amount to refund"); 
         }  
 
-        uint256 partialAmount = totalAmount - lastAmount; //El total menos la ultima apuesta es lo que puede recuperar.
-        bidAddressTotalAmount[msg.sender] -= partialAmount; //Resto de su total acumulado, lo que le voy a transferir para que no repita la operación n veces.
+		uint256 partialAmount = totalAmount - lastAmount; //El total menos la ultima apuesta es lo que puede recuperar.
+        bidAddressTotalAmount[msg.sender] -= partialAmount; //Resto el monto parcial (que se le va a devolver) de su total acumulado, así no puede pedir refund n veces.
         
-        (bool sent,) = payable(msg.sender).call{value: partialAmount}(""); //Transfiero
+		//Envío el dinero
+        (bool sent,) = payable(msg.sender).call{value: partialAmount}(""); 
         require(sent, "Failed to refund"); //Si hubo error, revierto.
-
-        
 
     }
 
